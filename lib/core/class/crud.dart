f786030099aac6fef9c9b5b33411e520406e9711
MosasaUnited <mostafa447@hotmail.com>
2023-06-25 22:43:47 +0300

    CRUD

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce/core/class/statusrequest.dart';
+import 'package:ecommerce/core/functions/check_internet.dart';
+import 'package:http/http.dart' as http;
+
+class Crud {
+  Future<Either<StatusRequest, Map>> postData(String linkurl, Map data) async {
+    try {
+      if (await checkInternet()) {
+        var response = await http.post(Uri.parse(linkurl), body: data);
+
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          Map responseBody = jsonDecode(response.body);
+          return Right(responseBody);
+        } else {
+          return const Left(StatusRequest.serverFailure);
+        }
+      } else {
+        return const Left(StatusRequest.offlineFailure);
+      }
+    } catch (_) {
+      return const Left(StatusRequest.serverFailure);
+    }
+  }
+}
