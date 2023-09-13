1071fd63673d783852ffbf32620bc19eea973223
MosasaUnited <mostafa447@hotmail.com>
2023-09-14 02:58:09 +0300

    HomePageP2

--- lib/core/constant/color.dart
+++ lib/core/constant/color.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-class AppColor{
+class AppColor {
   static const Color grey = Color(0xff676767);
   static const Color black = Color(0xff000000);
   static const Color backgroundColor = Color(0xffFFFFFF);
   static const Color primaryColor = Color(0xffFFAF4E);
+  static const Color secondColor = Color(0xffc0392b);
 }
--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/controller/home_contoller.dart';
+import 'package:ecommerce/core/constant/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -49,6 +50,48 @@ class HomePage extends StatelessWidget {
                       Icons.notifications_active_outlined,
                       size: 35,
                       color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Stack(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: AppColor.primaryColor,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: const ListTile(
+                    title: Text(
+                      'Summer Big Sale',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Cashback 20%',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: -35,
+                  right: -35,
+                  child: Container(
+                    height: 160,
+                    width: 160,
+                    decoration: BoxDecoration(
+                      color: AppColor.secondColor,
+                      borderRadius: BorderRadius.circular(160),
                     ),
                   ),
                 ),
