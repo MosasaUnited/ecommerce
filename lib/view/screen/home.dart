592e204ba24535c9de050b1a2aa0b0eebc146f85
MosasaUnited <mostafa447@hotmail.com>
2023-09-17 21:26:56 +0300

    PreparingCategories&Items&CategoriesModel

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -10,95 +10,97 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeControllerImp controller = Get.put(HomeControllerImp());
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              children: [
-                Expanded(
-                    child: TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search),
-                    hintText: 'Find Product',
-                    hintStyle: const TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 18,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                  ),
-                )),
-                const SizedBox(
-                  width: 10,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  width: 60,
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.notifications_active_outlined,
-                      size: 35,
-                      color: Colors.grey[400],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Stack(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      color: AppColor.primaryColor,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: const ListTile(
-                    title: Text(
-                      'Summer Big Sale',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: ListView(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search),
+                      hintText: 'Find Product',
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
                       ),
-                    ),
-                    subtitle: Text(
-                      'Cashback 20%',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                      filled: true,
+                      fillColor: Colors.grey[200],
                     ),
+                  )),
+                  const SizedBox(
+                    width: 10,
                   ),
-                ),
-                Positioned(
-                  top: -35,
-                  right: -35,
-                  child: Container(
-                    height: 160,
-                    width: 160,
+                  Container(
                     decoration: BoxDecoration(
-                      color: AppColor.secondColor,
-                      borderRadius: BorderRadius.circular(160),
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    width: 60,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.notifications_active_outlined,
+                        size: 35,
+                        color: Colors.grey[400],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: AppColor.primaryColor,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const ListTile(
+                      title: Text(
+                        'Summer Big Sale',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Cashback 20%',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: -35,
+                    right: -35,
+                    child: Container(
+                      height: 160,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        color: AppColor.secondColor,
+                        borderRadius: BorderRadius.circular(160),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
