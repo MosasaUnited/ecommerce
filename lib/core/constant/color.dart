6fa606be349ba1f848ed9e00b8e545c0dfdb5fda
MosasaUnited <mostafa447@hotmail.com>
2024-03-16 20:49:06 +0200

    AddAppIcon&ChangingPrimaryColor

--- lib/core/constant/color.dart
+++ lib/core/constant/color.dart
@@ -4,7 +4,7 @@ class AppColor {
   static const Color grey = Color(0xff676767);
   static const Color black = Color(0xff000000);
   static const Color backgroundColor = Color(0xffFFFFFF);
-  static const Color primaryColor = Color(0xffB7D9E8);
+  static const Color primaryColor = Color(0xff3EBDA8);
   static const Color secondColor = Color(0xffD6E3E9);
-  static const Color thirdColor = Color.fromARGB(255, 255, 179, 170);
+  static const Color thirdColor = Color(0xffB7D9E8);
 }
